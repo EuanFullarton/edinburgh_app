a465ee118376050103c95f35f0df8fcd84532514
Euan Fullarton <euanfullarton7@gmail.com>
2017-05-25 15:15:49 +0100

    Initial files created, seed data added to mongo - Euan

---
+++ db/seeds.js
@@ -0,0 +1,10 @@
+use Edinburgh;
+
+db.dropDatabase();
+
+db.places.insert({name:"Edinburgh Castle"})
+
+db.history.insert({Fact:"Edinburgh Castle is a historic fortress which dominates the skyline of the city of Edinburgh, Scotland, from its position on the Castle Rock."})
+
+db.places.find();
+db.history.find();
