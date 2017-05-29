a249b1e29afacc362e2b37651914e0cc475699d6
Euan Fullarton <euanfullarton7@gmail.com>
2017-05-29 11:44:35 +0100

    made some styling changes for mapView

--- client/src/views/mapView.js
+++ client/src/views/mapView.js
@@ -41,12 +41,17 @@ MapView.prototype = {
 
     var mapContainer = document.getElementById("map-container");
     mapContainer.innerHTML = "";
-    mapContainer.style.display = "block";
-    for(var place of places){
-      var p = document.createElement('p');
-      p.innerText = place.name;
-      mapContainer.appendChild(p);
-    }
+    // mapContainer.style.display = "block";
+    // for(var place of places){
+    //   var p = document.createElement('p');
+    //   p.innerText = place.name;
+    //   mapContainer.appendChild(p);
+    // }
+
+    var googleMap = new google.maps.Map(mapContainer, {
+    center: {lat: 55.953251, lng: -3.188267},
+    zoom: 10
+    });
   }
 }
 
