7ff223555aae2df02b219183782bdd713ea4511e
Tristan Gray <tristangraycc@gmail.com>
2017-05-30 14:41:15 +0100

    Pull error fixes

--- client/src/views/mapView.js
+++ client/src/views/mapView.js
@@ -1,20 +1,19 @@
 var MapItems = require('../models/mapItems');
 var MapItem = require('../models/mapItem');
-// var FavItems = require('../models/favItems');
+
 var MapView = function() {
+
 };
+
 MapView.prototype = {
   getMap:function(){
     var mapItems = new MapItems();
-    // var favItems = new FavItems();
     mapItems.all(function(places){
       this.renderMap(places);
       this.renderFavs(places);
     }.bind(this));
-    // favItems.all(function(favs){
-    //   this.renderFavs(favs);
-    // }.bind(this));
   },
+
   renderMap: function(places){
     var mapView = this;
     var mapContainer = document.getElementById("map-container");
@@ -33,13 +32,16 @@ MapView.prototype = {
     menuButton.style.display = "block";
     var navButtons = document.getElementsByClassName("nav-button");
     var mainButtons = document.getElementsByClassName("main-button");
+
     for (button of navButtons){
       button.style.margin = "0";
       button.style.padding = "1%";
     };
+
     for (button of mainButtons){
       button.style.display = "none";
     }
+
     var mainHeader = document.getElementById("main-header");
     mainHeader.style.display = "none";
     var historyHeader = document.getElementById("history-title");
@@ -59,8 +61,8 @@ MapView.prototype = {
       center: {lat: 55.953251, lng: -3.188267},
       zoom: 13
     });
-    for(var place of places){
 
+    for(var place of places){
       var p = document.createElement('p');
       p.innerText = place.name;
       mapContainer.appendChild(p);
@@ -96,6 +98,7 @@ MapView.prototype = {
       });
     }
   },
+
   renderFavs: function(places){
 
     var mapView = this;
@@ -103,7 +106,6 @@ MapView.prototype = {
     favsContainer.innerHTML = "";
     favsContainer.style.display = "block";
     for(var fav of places){
-      console.log("here:", places)
      if (fav.favourited){
         var favBox = document.createElement('div');
         var p1 = document.createElement('p');
@@ -111,15 +113,22 @@ MapView.prototype = {
         var deleteButton = document.createElement('BUTTON');
         p1.innerText = fav.name;
         p2.innerText = fav.info;
-        deleteButton.name = "Delete";
-        deleteButton.value = "Delete";
+        deleteButton.innerText = "Delete";
+        deleteButton.id = fav._id;
         favBox.appendChild(p1);
         favBox.appendChild(p2);
         favBox.appendChild(deleteButton);
         favsContainer.appendChild(favBox);
+
+        var deleteButton = document.getElementById(fav.name);
+ +
+ +      deleteButton.addEventListener('click', function(){
+ +        mapView.updateItem(fav._id);
+ +      });
      }
     };
   },
+
   // addToFavourites: function(place){
   //   var favItems = new FavItems();
   //   var callback = function(place){
